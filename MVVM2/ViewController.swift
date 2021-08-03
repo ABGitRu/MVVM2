348b61aa69939d4c60c25db0b3bb64862dc98158
Alexey Barinov <begoodbebad2012@gmail.com>
2021-08-03 10:42:34 +0300

    Initial Commit

---
+++ MVVM2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVVM2
+//
+//  Created by Mac on 03.08.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
